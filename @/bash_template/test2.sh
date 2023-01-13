27b1909eaff32b01c3b69ff9ab9286a65130cd73
nittics2002 <bunpabpab@gmail.com>
2023-01-14 06:52:14 +0900

    bash_template getoption OK

--- @/bash_template/test2.sh
+++ @/bash_template/test2.sh
@@ -2,6 +2,8 @@
 
 #set -x
 
+source var_dump.sh
+
 function version()
 {
     echo v0.0.1
@@ -32,44 +34,54 @@ EOL
 }
 
 
-SHORT_OPTION=hvn
-LONG_OPTION=help,version,dry-run
+SHORT_OPTION=hvnd:i:
+LONG_OPTION=help,version,dry-run,max-depth:,ignore:
 
 Params=$(getopt -o "${SHORT_OPTION}" -l "${LONG_OPTION}" -- "$@")
 eval set -- "${Params}"
 
-
-
-echo ${#Params[@]}
-
-#for x in "${Params[@]}" ;
-#for x in "$@" ;
-for x in ${Params[@]} ;
+while [[ $# -gt 0 ]] ;
 do
-    echo $x
-    
-    case "${x}" in
-        -h | --help)
+    case "$1" in
+        -h|--help)
             usage
             exit 0
             ;;
-        -v | --version)
+        -v|--version)
             version
             exit 0
             ;;
-        -n | --dry-run)
-            echo AAA;
+        -n|--dry-run)
+            shift
+            ;;
+        -d|--max-depth)
+            max_depth="$2"
+            shift 2
+            ;;
+        -i|--ignore)
+            ignore="$2"
+            shift 2
             ;;
         --)
+            shift
             break
-            echo BBB
             ;;
     esac
-
-    shift
 done
 
-echo ${#Params[@]}
+
+echo AAAAAAAAAAAAAAAAAa
+
+var_dump "$@"
+
+echo AAAAAAAAAAAAAAAAAa
+
+echo max_depth="${max_depth}"
+
+echo ignore="${ignore}"
 
 echo END
+
+var_dump "${Params[@]}"
+
 
---
+++ @/bash_template/var_dump.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+function var_dump()
+{
+    for x in "$@" ;
+    do
+        echo "${x}"
+    done
+}
+
