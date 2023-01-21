065866fe66056af59873586a43c211aa0585239b
nittics2002 <bunpabpab@gmail.com>
2023-01-21 10:00:25 +0900

    bash_xpath xpath parse検討中

--- @/bash_xpath/test03.sh
+++ @/bash_xpath/test03.sh
@@ -1,10 +1,64 @@
 #!/bin/bash
 #
-# 標準入力の<を改行,空行を削除
+# xpath式parse 
 #
 
-sed -E -e 's/</\n</g' | \
-    sed -E -e '/^$/d' | \
-    sed -E -e '/^\s+$/d'
+
+#
+# @param string &xpath
+# @param int &token_position
+# @return int moved_token_position
+#
+function parse_node()
+{
+    local -n xpath="$1"
+    local -n token_position="$2"
+    local -a tokens
+
+    while 1
+    do
+        if [[ -z ${xpath:$1:1} ]]
+            echo "${tokens[@]}"
+            return 0
+        fi
+
+        if [[ ${xpath:$1:1} =~ [A-Za-z1-6*
+
+    done
+
+    echo "${tokens[@]}"
+    return 0
+}
+
+
+
+
+
+function xpath_parse()
+{
+    local -i i
+    local -i node=0
+    local -a tokens
+    local -i token_position
+
+    for (( i=0; i<${#1}; i++ ))
+    do
+        #echo "${1:$i:1}"
+
+        case "${1:$i:1}" in
+            /)
+                node+=1
+                ;;
+
+
+
+
+
+    done
+}
+
+in=abcdefg
+
+xpath_parse "$in"
 
 
