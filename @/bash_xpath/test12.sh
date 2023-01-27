b81ff82917673d7dc0d000c736b056276fb24072
nittics2002 <bunpabpab@gmail.com>
2023-01-27 13:07:49 +0900

    bash_xpath parse_indet OK

---
+++ @/bash_xpath/test12.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#
+# 段落調査 
+#
+# 1行tagで終了記号 /> が無いtagの文ずれる
+#
+
+#set -x
+
+#空tag
+void_elements=( area base br col embd hr img input link meta source track wbr )
+
+
+#
+# 空tag判定
+#
+# @param 
+#
+function is_void_element()
+{
+    local pattern
+
+    for x in "${void_elements[@]}"
+    do
+        pattern=^\<${x}
+
+        [[ $1 =~ ${pattern} ]] && echo && return 0
+    done
+
+    echo false
+    return 1
+}
+
+declare -i indent=1
+
+tac | while read line
+do
+    #echo "${line}"
+    
+    #終了tag
+    if [[ ${line} =~ ^\</[A-Za-z0-9]+\> ]]
+    then
+        echo "${indent}"
+        (( indent++ ))
+    #空tag
+    elif [[ -z $(is_void_element ${line}) ]]
+    then
+        echo "${indent}"
+    #開始tag
+    elif [[ ${line} =~ ^\<[A-Za-z0-9]+.*/\> ]]
+    then
+        echo "${indent}"
+    #1行tag
+    elif [[ ${line} =~ ^\<[A-Za-z0-9]+.*\> ]]
+    then
+        echo "${indent}"
+        (( indent-- ))
+    #その他
+    else
+        echo "${indent}"
+    fi
+done |tac
+
+
+
+
+
+
