caae4fea88c09422a5b4d1c574a13ec396d9691d
nittics2002 <bunpabpab@gmail.com>
2023-01-27 15:43:13 +0900

    bash_xpath test15 node test作成中

---
+++ @/bash_xpath/test15.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+#
+# 軸方向を検索
+#
+
+#set -x
+
+declare -a absolute_axis
+#declare -a relative_axis
+declare -a full_syntax
+declare -a node_test
+
+
+#
+#
+# @param restructed_file
+# @param ...xpath
+#
+function lexcer()
+{
+    local file="$1"
+    shift
+    
+    local -i len
+
+    for path in "$@"
+    do
+
+
+        #絶対軸
+        if [[ ${#path} -gt 1 ]] && [[ ${path} =~ ^/ ]]
+        then
+            tag="${path:1}"
+            absolute_axis+=( "$(grep -E -i -n ^\<${tag} ${file})" )
+
+        #相対軸
+        elif [[ ${path} =~ ^/ ]]
+        then
+            #relative_axis+=( "$(grep -E -i -n ^\<${tag} ${file})" )
+            :
+
+        #完全構文
+        elif [[ ${path} =~ ^:: ]]
+        then
+            syntax="${path:2}"
+            full_syntax+=( "$(grep -E -i -n ^\<${syntax} ${file})" )
+
+
+        #ノードテスト
+        elif [[ ${path} =~ ^\[ ]]
+        then
+            
+            len=$(( ${#path} -2 ))
+            test1="${path:1}"
+            test2="${test1::${len}}"
+            formula=( $( IFS== ; echo "${test2}" ) )
+            
+            node_test+=( "$(grep -E -i -n ${test2} ${file})" )
+
+            echo "${formula}"
+            echo
+
+        fi
+
+
+
+
+
+    done
+
+}
+
+
+file=./ddd.htm
+paths="/!DOCTYPE /html / /head /parent ::body [@id=\"pg-index\"]"
+
+lexcer ${file} ${paths}
+
+echo ----absolute
+echo "${absolute_axis}"
+echo
+
+
+echo ----full syntax
+echo "${full_syntax}"
+echo
+
+
+echo ----node_test
+echo "${node_test}"
+echo
+
+
+
+
